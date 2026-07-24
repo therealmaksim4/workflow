5b24f4fab458a4ad790b9fef0eff9472a9ae738e
therealmaksim4 <maksim.budim11052013@gmail.com>
2026-07-24 14:18:36 +0200

    Added command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
-int main(){
+int main(int argc, char *argv[]){
+    if(argc == 2 && argv[1] == "help"){
+        std::cout << "workflow v0.0.2" << std::endl << std::endl;
+        std::cout << "help - Display this help command" << std::endl;
+        std::cout << "cheat - Pull up cheatsheets" << std::endl;
+        std::cout << "man - Pull up man pages" << std::endl;
+        std::cout << std::endl << "The git repo: \"https://github.com/therealmaksim4/workflow\"" << std::endl;
+
+        return 0;
+    }
+
+    else if(argc == 2 && argv[1] == "cheat"){
+        system("bash ~/.config/workflow/src/cheatsheets/cheatsheets.sh");
+
+        return 0;
+    }
+
+    else if(argc == 2 && argv[1] == "man"){
+        system("bash ~/.config/workflow/src/manpages/manpages.sh");
+
+        return 0;
+    }
+
+    else if(argc > 2){
+        std::cout << "Too much arguments given" << std::endl;
+
+        return 1;
+    }
+
     system("clear");
 
     bool running = true;
