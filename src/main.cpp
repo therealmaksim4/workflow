e5e1891d750cf9d2a0ed7f1b3fd10c981c2a4867
therealmaksim4 <maksim.budim11052013@gmail.com>
2026-07-26 17:59:37 +0200

    Removed command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,36 @@
 #include <iostream>
 
 int main(int argc, char *argv[]){
-    if(argc == 2 && argv[1] == "help"){
-        std::cout << "workflow v0.0.2" << std::endl << std::endl;
-        std::cout << "help - Display this help command" << std::endl;
-        std::cout << "cheat - Pull up cheatsheets" << std::endl;
-        std::cout << "man - Pull up man pages" << std::endl;
-        std::cout << std::endl << "The git repo: \"https://github.com/therealmaksim4/workflow\"" << std::endl;
+    system("clear");
 
-        return 0;
-    }
+    bool running = true;
+    std::string cmd;
 
-    else if(argc == 2 && argv[1] == "cheat"){
-        system("lua ~/.config/workflow/lua/cheat.lua");
+    while(running){
+        std::cin >> cmd;
 
-        return 0;
-    }
+        if(cmd == "cheat"){
+            system("lua ~/.config/workflow/lua/cheat.lua");
+        }
 
-    else if(argc == 2 && argv[1] == "man"){
-        system("lua ~/.config/workflow/lua/man.lua");
+        else if(cmd == "man"){
+            system("lua ~/.config/workflow/lua/man.lua");
+        }
 
-        return 0;
-    }
+        else if(cmd == "bash"){
+            system("lua ~/.config/workflow/lua/bash.lua");
+        }
 
-    else if(argc > 2){
-        std::cout << "Too much arguments given" << std::endl;
+        else if(cmd == "clear"){
+            system("clear");
+        }
 
-        return 1;
-    }
-    
-    else if(argc == 1){
-        system("clear");
+        else if(cmd == "exit"){
+            running = false;
+        }
 
-        bool running = true;
-        std::string cmd;
-
-        while(running){
-            std::cin >> cmd;
-
-            if(cmd == "cheat"){
-                system("lua ~/.config/workflow/lua/cheat.lua");
-            }
-
-            else if(cmd == "man"){
-                system("lua ~/.config/workflow/lua/man.lua");
-            }
-
-            else if(cmd == "bash"){
-                system("lua ~/.config/workflow/lua/bash.lua");
-            }
-
-            else if(cmd == "clear"){
-                system("clear");
-            }
-
-            else if(cmd == "exit"){
-                running = false;
-            }
-
-            else{
-                std::cout << "Invalid command." << std::endl;
-            }
+        else{
+            std::cout << "Invalid command." << std::endl;
         }
     }
 
