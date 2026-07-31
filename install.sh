66e9d62eb0e2a17095df85dadc3f0d45fa246dc8
therealmaksim4 <maksim.budim11052013@gmail.com>
2026-07-31 19:17:25 +0200

    install.sh will only show errors if there is a problem with CMake

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ echo "Making the config directory... "
 
 cd
 
-mkdir -p ~/.config/workflow 1>/dev/null
+mkdir -p ~/.config/workflow > /dev/null 2>&1
 
 echo "Removing past config files... "
 
-rm -rf ~/.config/workflow/lua 1>/dev/null
+rm -rf ~/.config/workflow/lua > /dev/null 2>&1
 
 echo "Copying the lua source code into the config directory... "
 
@@ -18,10 +18,10 @@ echo "Building and installing the files... "
 
 cd ~/workflow
 
-mkdir build
+mkdir build > /dev/null 2>&1
 
 cmake -S . -B build 1>/dev/null
 cmake --build build 1>/dev/null
 sudo cmake --install build 1>/dev/null
 
-rm -rf build
+rm -rf build > /dev/null 2>&1
